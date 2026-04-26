875f3aff1e3c58c84f142f34aab928dc356572cd
Chiranjeevi Mishra <chiranjeevimishra1998@gmail.com>
2026-04-26 11:15:00 -0500

    Update 0_downloadSRA_raw_data_cancer_mammary.sh

--- 0_downloadSRA_raw_data_cancer_mammary.sh
+++ 0_downloadSRA_raw_data_cancer_mammary.sh
@@ -1,54 +1,46 @@
 #!/bin/bash
 # =============================================================================
-# Complete fastq files download steps: Cancer vs Normal (Dog Cancer Mammary Tissue)
+# Script: Download FASTQ Files from NCBI SRA
+# Project: Canine Mammary Tumor RNA-seq Analysis
 # Author: Chiranjeevi Mishra | Auburn University | April 2026
-# ALL OUTPUTS SAVED TO "group_project" FOLDER 
+#
+# Description:
+# This script downloads paired-end RNA-seq FASTQ files for canine mammary
+# tumor samples from NCBI SRA using the SRA Toolkit.
+#
+# Input:
+#   - SRA Run IDs from BioProject PRJNA489087
+#
+# Output:
+#   - Paired-end FASTQ files saved in:
+#     /scratch/${MyID}/Project/rawfile_1
+#
+# HPC Resources:
+#   - Queue: class
+#   - CPUs: 1
+#   - Memory: 4 GB
+#   - Walltime: 04:00:00
+#   - Run on: asax
 # =============================================================================
 
-## 	download data from NCBI SRA using the SRAtoolkit and the SRA run IDs: https://www.ncbi.nlm.nih.gov/sra/docs/sradownload/
-## 	use FASTQC to evaluate the quality of the data: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
-## Download from SRA: Input Data: NA
-## 		queue: class
-##		core: 1
-##		time limit (HH:MM:SS): 04:00:00 
-##		Memory: 4gb
-##		run on asax
-###############################################
-
-
-########## Load Modules
+# Load required modules
 source /apps/profiles/modules_asax.sh.dyn
 module load sra
 
-##########  Define variables and make directories
-## Replace the numbers in the brackets with Your specific information
-  ## make variable for your ASC ID so the directories are automatically made in YOUR directory
-  ## These are represented in the code by [#] replace these according to the examples provided
-MyID=aubclsf0047          
+# Define variables
+MyID=aubclsf0047
+DD=/scratch/${MyID}/Project/rawfile_1
 
-  ## Make variable that represent YOUR working directory(WD) in scratch, your Raw data directory (DD) and the pre or postcleaned status (CS).
-DD=/scratch/${MyID}/Project/rawfile_1			## Example: DD=/scratch/${MyID}/PracticeRNAseq/RawData
- 
-##  make the directories in SCRATCH for holding the raw data 
-## -p tells it to make any upper level directories that are not there. Notice how this will also make the WD.
+# Create raw data directory and move into it
 mkdir -p ${DD}
-## move to the Data Directory
 cd ${DD}
 
-##########  Download data files from NCBI: SRA using the Run IDs
-  ### from SRA use the SRA tool kit - see NCBI website https://www.ncbi.nlm.nih.gov/sra/docs/sradownload/
-	## this downloads the SRA file and converts to fastq
-	## -F 	Defline contains only original sequence name.
-	## -I 	Append read id after spot id as 'accession.spot.readid' on defline.
-	## --split-files splits the files into R1 and R2 (forward reads, reverse reads)
-
-## We must run vdb-config to force creation of the default config file, otherwise we will get an error. This is a 'hack'. 
-
+# Initialize SRA Toolkit configuration
 vdb-config --interactive > /dev/null 2>&1 <<EOF
 q
 EOF
 
-
+# Download paired-end FASTQ files from NCBI SRA
 fastq-dump -F --split-files SRR7819851
 fastq-dump -F --split-files SRR7819863
 fastq-dump -F --split-files SRR7819867
@@ -60,13 +52,3 @@ fastq-dump -F --split-files SRR7819885
 fastq-dump -F --split-files SRR7819891
 fastq-dump -F --split-files SRR7819893
 
-##### Extra ####
-## If you are downloading data from a sequencing company instead of NCBI, using wget for example, then calculate the md5sum values of all the files in the folder (./*), and read into a text file.
-## then you can compare the values in this file with the ones provided by the company.
-#md5sum ./* > md5sum.txt
-
-##### Extra ####
-## If you data comes with multiple R1 and R2 files per individual. You can contatenate them together using "cat" before running FASTQC
-## see examples below for one file. You will probably want to use a loop to process through all the files.
-#cat SRR6819014*_R1_*.fastq.gz > SRR6819014_All_R1.fastq.gz
-#cat SRR6819014*_R2_*.fastq.gz > SRR6819014_All_R2.fastq.gz
